c4702c77037e0c4a444aba8964e3bbf2218d1c91
Ayush Tankar <ayushtankar143@gmail.com>
2024-08-11 09:56:43 +0530

    Added Prime Number Program

---
+++ prime.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    bool flag = 0;
+
+    cout << "Enter the Number" << endl;
+    cin >> num;
+
+    for (int i = 2; i <= sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            cout << "Non-Prime Number" << endl;
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+    {
+        cout << "Prime Number" << endl;
+    }
+    return 0;
+}
